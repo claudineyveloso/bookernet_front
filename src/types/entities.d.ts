8791f3b9bc9ae33b434942ad49ec6ab21b994c35
Claudiney Veloso <claudineyveloso@gmail.com>
2024-04-16 19:14:22 -0300

    added types Owner, Person and Address

--- src/types/entities.d.ts
+++ src/types/entities.d.ts
@@ -9,6 +9,38 @@ declare namespace Entities {
     aws_region?: string;
   };
 
+  type Owner = {
+    id?: string;
+    people_type: string;
+    is_active: boolean;
+    bucket_id: string;
+    person: Person;
+    address: Address;
+  };
+
+  type Person = {
+    id?: string;
+    firt_name: string;
+    last_name: string;
+    email: string;
+    phone: string;
+    cell_phone: string;
+    personable_id: string;
+    personable_type: string;
+  };
+
+  type Address = {
+    id?: string;
+    public_place: string;
+    complement: string;
+    neighborhood: string;
+    city: string;
+    state: string;
+    zip_code: string;
+    addressable_id: string;
+    addressable_type: string;
+  };
+
   type User = {
     id?: string;
     email?: string;
