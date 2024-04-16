8dcef4bc9a632a786bf8f6314a9d92609f09c6f0
Claudiney Veloso <claudineyveloso@gmail.com>
2024-04-16 19:15:43 -0300

    created crud owner

---
+++ src/app/owner/page.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Toast } from 'primereact/toast';
+import { Toolbar } from 'primereact/toolbar';
+import { Button } from 'primereact/button';
+import { Column } from 'primereact/column';
+import { DataTable } from 'primereact/datatable'
+import { Dialog } from 'primereact/dialog';
+import { InputText } from 'primereact/inputtext';
+import { InputTextarea } from 'primereact/inputtextarea';
